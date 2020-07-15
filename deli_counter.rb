fdb5fc48ab38e415c7028c2abce8eff81de5446f
Fatou Diagne <f.m.diagne1@gmail.com>
2020-07-15 21:43:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,14 +3,14 @@ def line(deli)
     puts "The line is currently empty."
   else 
     current_line = "The line is currently:" 
-    deli.each.with_index(1) do |customer, number|
+    deli.each.with_index(1) do |customer,number|
       current_line << "#{number}. #{customer}"
 end
   puts current_line
 end
 end
 
-def take_a_number(deli, name)
+def take_a_number(deli,name)
   deli << name
   puts "Welcome, #{name}. You are number #{deli.length} in line."
 end
